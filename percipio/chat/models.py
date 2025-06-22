f3276b5b29a304b820fd405fbe2f9dc09148c4f6
saumy <saumyasri1914@gmail.com>
2025-06-22 22:07:34 +0530

    chat model done

--- percipio/chat/models.py
+++ percipio/chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name='received_messages')  # for 1-1
+    project = models.ForeignKey('tasks.Project', null=True, blank=True, on_delete=models.CASCADE)  # for group chat
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
