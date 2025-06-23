9fdfbaa6920a36617cb540b3998e00c02ca0cc29
saumy <saumyasri1914@gmail.com>
2025-06-23 22:34:50 +0530

    channels setup

--- percipio/chat/models.py
+++ percipio/chat/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.conf import settings
+from tasks.models import Project
 
 class ChatMessage(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name='received_messages')  # for 1-1
-    project = models.ForeignKey('tasks.Project', null=True, blank=True, on_delete=models.CASCADE)  # for group chat
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name='received_messages') 
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
--- percipio/percipio/asgi.py
+++ percipio/percipio/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.urls
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'percipio.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.urls.websocket_urlpatterns
+        )
+    ),
+})
