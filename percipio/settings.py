13a88529f24fdc504da40eb399e996ec6984661f
saumy <saumyasri1914@gmail.com>
2025-07-01 23:57:22 +0530

    changing db

--- percipio/settings.py
+++ percipio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2gynxnvk6*iq$r#q+^k@@52h0tf9qiut)2rr%p(((0yi0gq$aa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
